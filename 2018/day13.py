16610bcfd07cbbf604e995429220dd6b8598c3bb
Jamie Ly <jamie.ly@gmail.com>
2018-12-25 11:48:12 -0500

    Initial loading of single track

--- 2018/day13.py
+++ 2018/day13.py
@@ -19,34 +19,43 @@
 # |    |
 # |    |
 # \----/
+class TrackType:
+    Empty = '.'
+    UpDown = '|'
+    LeftRight = '-'
+    RightTurn = 'R'
+    LeftTurn = 'L'
+    Intersection = '+'
+
 class Dir:
-    Empty = 0
-    UpDown = 5
-    LeftRight = 1
-    RightTurn = 2
-    LeftTurn = 3
-    Intersection = 4
+    Up = (-1, 0)
+    Down = (1, 0)
+    Left = (0, -1)
+    Right = (0, 1)
 
 class Day13:
     CHAR_DIR_MAPPINGS = {
-        '|': Dir.UpDown,
-        '-': Dir.LeftRight,
-        '\\': Dir.RightTurn,
-        '/': Dir.LeftTurn,
-        '+': Dir.Intersection
+        '|': TrackType.UpDown,
+        '-': TrackType.LeftRight,
+        '\\': TrackType.RightTurn,
+        '/': TrackType.LeftTurn,
+        '+': TrackType.Intersection
     }
 
     DIR_CHAR_MAPPINGS = {
-        Dir.Empty: ' ',
-        Dir.UpDown: '|',
-        Dir.LeftRight: '-',
-        Dir.RightTurn: '\\',
-        Dir.LeftTurn: '/',
-        Dir.Intersection: '+'
+        TrackType.Empty: ' ',
+        TrackType.UpDown: '|',
+        TrackType.LeftRight: '-',
+        TrackType.RightTurn: '\\',
+        TrackType.LeftTurn: '/',
+        TrackType.Intersection: '+'
     }
 
+    def __init__(self):
+        self.track_counter = 0
+
     def parse_char(self, char):
-        return Day13.CHAR_DIR_MAPPINGS[char] if char in Day13.CHAR_DIR_MAPPINGS else Dir.Empty
+        return Day13.CHAR_DIR_MAPPINGS[char] if char in Day13.CHAR_DIR_MAPPINGS else TrackType.Empty
 
     def parse_line(self, line):
         return [self.parse_char(c) for c in line]
@@ -55,19 +64,132 @@ class Day13:
         "loads the input into a two-dimensional array and translates strings to types of pieces"
         return [self.parse_line(line) for line in input.strip().splitlines()]
 
+    def load_tracks(self, parsed):
+        "Loads all the tracks in the input"
+        # we go through each column, then each row, fniding new tracks. if we
+        # find one, we follow it to the end
+        track_index = [[None for col in row] for row in parsed]
+        tracks = []
+        state = {
+            'track_index': track_index,
+            'tracks': tracks
+        }
+        for row in range(0, len(parsed)):
+            for col in range(0, len(parsed[row])):
+                if parsed[row][col] == TrackType.Empty: continue
+                if state['track_index'][row][col] is not None: continue
+                track = self.load_track(parsed, state, (row, col))
+                if track: tracks.append(track)
+                if state['track_index'][row][col] is None:
+                    state['track_index'][row][col] = []
+
+        return state
+
+    FOLLOW_TRACK_INITIAL_DIR = {
+        TrackType.LeftRight: Dir.Right,
+        TrackType.UpDown: Dir.Down
+    }
+
+    DIR_STR = {
+        Dir.Down: 'D',
+        Dir.Up: 'U',
+        Dir.Left: 'L',
+        Dir.Right: 'R'
+    }
+
+    NEXT_DIR = {
+        Dir.Down: {
+            TrackType.UpDown: Dir.Down,
+            TrackType.Intersection: Dir.Down,
+            TrackType.RightTurn: Dir.Right,
+            TrackType.LeftTurn: Dir.Left
+        },
+        Dir.Up: {
+            TrackType.UpDown: Dir.Up,
+            TrackType.RightTurn: Dir.Left,
+            TrackType.LeftTurn: Dir.Right,
+            TrackType.Intersection: Dir.Up
+        },
+        Dir.Left: {
+            TrackType.LeftRight: Dir.Left,
+            TrackType.Intersection: Dir.Left,
+            TrackType.LeftTurn: Dir.Down,
+            TrackType.RightTurn: Dir.Up
+        },
+        Dir.Right: {
+            TrackType.LeftRight: Dir.Right,
+            TrackType.Intersection: Dir.Right,
+            TrackType.LeftTurn: Dir.Up,
+            TrackType.RightTurn: Dir.Down
+        }
+    }
+
+    def add_indices(self, a, b):
+        r1, c1 = a
+        r2, c2 = b
+        return (r1 + r2, c1+ c2)
+
+    def out_of_bounds(self, parsed, index):
+        row, col = index
+        if row >= len(parsed): return True
+        if col >= len(parsed[row]): return True
+        return False
+
+    def load_track(self, parsed, state, index, last_dir = None):
+        # follow the track until we reach the original position
+        original_index = index
+        track_indices = []
+
+        while True:
+            if self.out_of_bounds(parsed, index):
+                print(f'out of bounds!! index={index}')
+                break
+
+            row, col = index
+            track_type = parsed[row][col]
+            if last_dir is None and (track_type == TrackType.LeftTurn or track_type == TrackType.RightTurn):
+                print('Cannot process turn as first track')
+                return None
+
+            if last_dir is None:
+                track_id = self.track_counter
+                self.track_counter += 1
+                last_dir = Day13.FOLLOW_TRACK_INITIAL_DIR[track_type]
+                print(f'last_dir empty so initialized to {Day13.DIR_STR[last_dir]} using track_type={track_type}')
+
+            if last_dir not in Day13.NEXT_DIR:
+                print(f'bad last_dir={last_dir}')
+            elif track_type not in Day13.NEXT_DIR[last_dir]:
+                print(f'bad track_Type={track_type} from last_dir={Day13.DIR_STR[last_dir]}')
+            else:
+                next_dir = Day13.NEXT_DIR[last_dir][track_type]
+
+            track_indices.append(index)
+
+            if not state['track_index'][row][col]:
+                state['track_index'][row][col] = []
+
+            state['track_index'][row][col].append(track_id)
+
+            next_index = self.add_indices(index, next_dir)
+            print(f'id={track_id} index={index} next_index={next_index} track_type={track_type} last_dir={Day13.DIR_STR[last_dir]} next_dir={Day13.DIR_STR[next_dir]}')
+            if next_index == original_index:
+                # we reached the original index
+                print('Reached first track piece')
+                break
+            index = next_index
+            last_dir = next_dir
+        
+        return {
+            'starting_index': index,
+            'indicies': track_indices,
+            'id': track_id
+        }
+
+
     def render_raw(self, raw):
         return "\n".join(["".join([Day13.DIR_CHAR_MAPPINGS[char] for char in row]) for row in raw])
 
-    # def load_tracks(self, input):
-    #     "Loads all tracks in the passed input"
-    #     return []
-
-    # def load_track(self, index):
-    #     "Follows the track at the passed index until the entire track is loaded"
-    #     # a track is a list of indices
-    #     return {
-    #         'indices': []
-    #     }
 # 
 # Intersections occur when two perpendicular paths cross. At an intersection, a
 # cart is capable of turning left, turning right, or continuing straight. Here
--- 2018/day13.test.py
+++ 2018/day13.test.py
@@ -9,11 +9,38 @@ class TestDay13(unittest.TestCase):
         print(rendered)
         self.assertEqual(rendered, TestDay13.input1.strip())
 
+    def test_load_tracks_1(self):
+        day = Day13()
+        parsed = day.parse_input(TestDay13.input1)
+        track_info = day.load_tracks(parsed)
+        self.assertEqual(len(track_info['tracks']), 1)
+        print(track_info['tracks'][0])
+        self.assertEqual(len(track_info['tracks'][0]['indicies']), 16)
+        
+    def test_load_tracks_2(self):
+        day = Day13()
+        parsed = day.parse_input(TestDay13.input2)
+        print(day.render_raw(parsed))
+        track_info = day.load_tracks(parsed)
+        self.assertEqual(len(track_info['tracks']), 2)
+        self.assertEqual(len(track_info['tracks'][0]['indicies']), 20)
+        self.assertEqual(len(track_info['tracks'][1]['indicies']), 20)
+
     input1 = """
 /----\\
 |    |
 |    |
 \----/
+"""
+
+    input2 = """
+/-----\\
+|     |
+|  /--+--\\
+|  |  |  |
+\--+--/  |
+   |     |
+   \-----/
 """
 
 if __name__ == "__main__":
